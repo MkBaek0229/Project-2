b87f2f112cc370ff6cf804bd5da91cbc419ad5ed
MkBaek0229 <alsrl6678@naver.com>
2024-11-05 20:54:30 +0900

    더미데이터생성

--- 4기 백민기/components/SidebarPage.js
+++ 4기 백민기/components/SidebarPage.js
@@ -1,5 +1,11 @@
-export default function SidebarPage({ $target }) {
+export default function SidebarPage({ $target, initalState }) {
+    this.state = initalState
+
     const $page = document.createElement('div')
-    $page.innerHTML = 'sidebar'
+    $page.innerHTML = `
+    <ul>
+        ${this.state.map((row) => `<li>${row.id} / ${row.title}</li>`).join('')}
+    </ul>
+`
     $target.appendChild($page)
 }
