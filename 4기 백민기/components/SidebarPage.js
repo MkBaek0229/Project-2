9ae3be6f268aea02aa2807659625dcde9a447797
MkBaek0229 <alsrl6678@naver.com>
2024-11-05 21:06:09 +0900

    재귀형태 출력 구현

--- 4기 백민기/components/SidebarPage.js
+++ 4기 백민기/components/SidebarPage.js
@@ -1,11 +1,54 @@
 export default function SidebarPage({ $target, initalState }) {
     this.state = initalState
 
+    this.createTreeView = (data) => {
+        let str = ''
+        for (const key in data) {
+            if (data[key].documents.length > 0) {
+                str += `
+                    <li class="dataList">
+                        📄 ${data[key].title}
+                        <button class="addBtn">➕</button>
+                        <button class="delBtn">🗑️</button>
+                        <ul>${this.createTreeView(data[key].documents)}</ul>
+                    </li>
+               `
+            } else {
+                str += `
+                <li class="dataList">
+                    📄 ${data[key].title}
+                    <button class="addBtn">➕</button>
+                    <button class="delBtn">🗑️</button>
+                </li>
+           `
+            }
+        }
+        return str
+    }
     const $page = document.createElement('div')
+    $page.classList.add('listContainer')
     $page.innerHTML = `
-    <ul>
-        ${this.state.map((row) => `<li>${row.id} / ${row.title}</li>`).join('')}
-    </ul>
-`
+        <ul>
+            ${this.state
+                .map(
+                    (document) =>
+                        `<li class="dataList">📄 ${document.title}
+                        <button class="addBtn">➕</button>
+                        <button class="delBtn">🗑️</button>
+                    </li>
+                    ${
+                        document.documents.length > 0
+                            ? `<ul>${this.createTreeView(
+                                  document.documents
+                              )} </ul>`
+                            : ''
+                    }
+                    
+                    
+                    `
+                )
+                .join('')}
+        </ul>
+    `
     $target.appendChild($page)
 }
